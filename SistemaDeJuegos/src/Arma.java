6fa8a6f6387b385b9f447120ed0cdbc89afd20f1
Jhon Carvajal <jcarvajal@smart-financial-systems.com>
2025-04-11 20:17:15 -0500

    Se crean interfaces, y las clases guerreros

---
+++ SistemaDeJuegos/src/Arma.java
@@ -0,0 +1,13 @@
+public class Arma {
+
+    private String nombre;
+    private int dañoAdicional;
+
+    public void atacarConArma(Criatura objetivo){
+
+    }
+
+    public int getDañoAdicional(){
+        return 0;
+    }
+}
---
+++ SistemaDeJuegos/src/Mago.java
@@ -0,0 +1,24 @@
+public class Mago extends Criatura implements Magico{
+
+    private String hechizos;
+
+    @Override
+    public void lanzarHechizo() {
+
+    }
+
+    @Override
+    public void aprenderHechizo() {
+
+    }
+
+    @Override
+    public void atacar(Criatura objetivo) {
+
+    }
+
+    @Override
+    public void defender(int daño) {
+
+    }
+}
