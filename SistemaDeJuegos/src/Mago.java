8fece8703ce5ed5c4a0585a80de7a0c1a2f2c7c6
Jhon Carvajal <jcarvajal@smart-financial-systems.com>
2025-04-12 23:37:46 -0500

    Se crea la lógica para la batalla

--- SistemaDeJuegos/src/Mago.java
+++ SistemaDeJuegos/src/Mago.java
@@ -1,24 +1,39 @@
-public class Mago extends Criatura implements Magico{
+public class Mago extends Criatura implements Magico {
 
     private String hechizos;
+    private Arma arma;
+
+    public Mago(String nombre, int salud, int fuerza, String hechizos) {
+        super(nombre, salud, fuerza);
+        this.hechizos = hechizos;
+    }
 
     @Override
     public void lanzarHechizo() {
-
+        System.out.println("Ataca con hechizo: " + hechizos);
     }
 
     @Override
     public void aprenderHechizo() {
-
+        System.out.println(hechizos + " ha aprendido un nuevo hechizo.");
     }
 
     @Override
     public void atacar(Criatura objetivo) {
+        int daño = getFuerza();
+        if (arma != null) {
+            daño += arma.getDañoAdicional();
+        }
+        System.out.println(getNombre() + " ataca a " + objetivo.getNombre() + " causando " + daño + " de daño.");
+        objetivo.defender(daño);
 
     }
 
     @Override
     public void defender(int daño) {
-
+        int saludActual = getSalud();
+        saludActual -= daño;
+        setSalud(saludActual);
+        System.out.println(getNombre() + " recibe " + daño + " de daño. Salud actual: " + getSalud());
     }
 }
--- SistemaDeJuegos/src/Main.java
+++ SistemaDeJuegos/src/Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+    public static void main(String[] args) {
+        //Dragon
+        Dragon guerreroDragon = new Dragon("Dragon Occidental", 100, 20);
+        //Mago
+        Mago magoHechizero = new Mago("Mago de oz", 100, 35, "Veneno");
+
+        //Método que simula la batalla entre las criaturas
+        simularBatalla(guerreroDragon, magoHechizero);
+    }
+
+    public static void simularBatalla(Criatura criatura1, Criatura criatura2) {
+        System.out.println("¡Comienza la batalla entre " + criatura1.getNombre() + " y " + criatura2.getNombre() + "!");
+
+        //Ciclo repetitivo que está pendiente si alguna de las criaturas está viva
+        //para seguir con la batalla
+        while (criatura1.estaViva() && criatura2.estaViva()) {
+            criatura1.atacar(criatura2);
+            if (!criatura2.estaViva() || !criatura1.estaViva()) break;
+            criatura2.atacar(criatura1);
+        }
+
+        //Condición que revisa si la criatura está viva, devuelve true si la salud es mayor que cero
+        //de lo contrario devuelve false
+        if (criatura1.estaViva()) {
+            System.out.println(criatura1.getNombre() + " ha ganado la batalla.");
+        } else {
+            System.out.println(criatura2.getNombre() + " ha ganado la batalla.");
         }
     }
 }
